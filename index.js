29729c0191719ad0e4681a31803fa51189a1d7b6
Keenan <keenanhallas@gmail.com>
2020-09-02 14:49:38 -0500

    logger middleware working

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require("./server");
+
+server.listen(3333, () => {
+    console.log("server up...");
+  });
--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require('express');
 
 const server = express();
 
+server.use(logger());
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger() {
+  return function (req, res, next) {
+    console.log(`${req.method} request to http://localhost:3333${req.url} on ${new Date()}`);
+    next();
+  }
+}
 
 module.exports = server;
